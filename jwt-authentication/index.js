2a25fd63ddaffd8b7fe0256d42651c1d70091e32
aelshh <chaudharyadarsh9919@gmail.com>
2025-04-19 08:33:24 +0530

    add jwt-authetication learnings

---
+++ jwt-authentication/index.js
@@ -0,0 +1,16 @@
+const jwt = require("jsonwebtoken");
+
+const secret = "someranadomsecreat";
+
+const value = {
+  fname: "Rakesh",
+  accNum: 2341243213141,
+};
+
+const token = jwt.sign(value, secret);
+console.log(token);
+
+const verfiy = jwt.verify(token, secret);
+const verfiy2 = jwt.verify(token, "someanotherrandomsecreat");
+
+console.log(verfiy);
